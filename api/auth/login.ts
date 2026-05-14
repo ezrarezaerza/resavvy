3061fa26beffedd1f13f76368aaa4ee72e2b72ed
ezrarezaerza <ezrarezaerza@gmail.com>
2026-05-15 03:12:25 +0700

    auth

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { prisma } from '../../src/lib/prisma'; // Make sure this path is pointing to src/lib/prisma
